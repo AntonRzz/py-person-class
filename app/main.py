35a07cdf0d77c30864d06f1160a445086f6bf7ed
Anton <anton-flash@ukr.net>
2024-12-18 11:19:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,16 +4,27 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[self.name] = self
-        
+        Person.people[name] = self
+
+    def __str__(self) -> str:
+        return f"Person(name={self.name}, age={self.age})"
+
 
 def create_person_list(people: list) -> list:
-    persons: list[Person] = []
-    for person in people:
-        persons.append(Person(person["name"], person["age"]))
-    for person in people:
-        if "wife" in person and person["wife"] is not None:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-            Person.people[
-                person["wife"]].husband = Person.people[person["name"]]
-    return persons
+    person_list = [
+        Person(name=person["name"], age=person["age"])
+        for person in people
+    ]
+    for person_data, person_instance in zip(people, person_list):
+        wife_name = person_data.get("wife")
+        husband_name = person_data.get("husband")
+
+        wife = Person.people.get(wife_name)
+        husband = Person.people.get(husband_name)
+
+        if wife:
+            person_instance.wife = wife
+
+        if husband:
+            person_instance.husband = husband
+    return person_list
